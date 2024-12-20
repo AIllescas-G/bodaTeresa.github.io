1ab98bc8ac17b61137b7ec2a111cc5c13b46088f
Antonio Illescas González <illescascorrecto@gmail.com>
2024-12-20 09:11:48 +0100

    todo bien

--- src/04-components/09-museo/Museo.jsx
+++ src/04-components/09-museo/Museo.jsx
@@ -3,7 +3,7 @@ import img1 from './museo.png';
 export default function Museo () {
   return (
     <div className="museo">
-    <img className="img" src={img1} alt="Imagen de Japón" />
+    <img className="img-museo" src={img1} alt="Imagen de Japón" />
   </div>
   )
 }
